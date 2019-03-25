f8e0c05bcd85ba760a4802da9df6d7e533d6e9b3
Santiago <pan@qelp.com>
2019-03-25 08:33:50 +0100

    Reorganise code in folders.

--- index.js
+++
@@ -1,5 +0,0 @@
-import * as p2 from './problem2/problem'
-import * as p4 from './problem4/problem'
-import * as p6 from './problem6/problem'
-import * as p8 from './problem8/problem'
-import * as p10 from './problem10/problem'
