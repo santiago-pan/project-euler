388f6bcb58d80776701f88db591e1ed3899acb77
Santiago <pan@qelp.com>
2019-04-28 21:01:52 +0200

    Improved version of problem 31.

--- python/problem31/problem.py
+++ python/problem31/problem.py
@@ -4,35 +4,12 @@ import time
 def solution():
     total = 1  # Since 2f is the first option
     for a in range(0, 3):
-        sum = a*100
-        if (sum > 200):
-            break
-        for b in range(0, 5):
-            sum = a*100 + b*50
-            if (sum > 200):
-                break
-            for c in range(0, 11):
-                sum = a*100 + b*50 + c*20
-                if (sum > 200):
-                    break
-                for d in range(0, 21):
-                    sum = a*100 + b*50 + c*20 + d*10
-                    if (sum > 200):
-                        break
-                    for e in range(0, 41):
-                        sum = a*100 + b*50 + c*20 + d*10 + e*5
-                        if (sum > 200):
-                            break
-                        for f in range(0, 101):
-                            sum = a*100 + b*50 + c*20 + d*10 + e*5 + f*2
-                            if (sum > 200):
-                                break
-                            for g in range(0, 201):
-                                sum = a*100 + b*50 + c*20 + d*10 + e*5 + f*2 + g*1
-                                if (sum > 200):
-                                    break
-                                if (sum == 200):
-                                    total += 1
+        for b in range(0, 1+(200-100*a)/50):
+            for c in range(0, 1+(200-100*a-50*b)/20):
+                for d in range(0, 1+(200-100*a-50*b-20*c)/10):
+                    for e in range(0, (1+(200-100*a-50*b-20*c-10*d)/5)):
+                        for f in range(0, (1+(200-100*a-50*b-20*c-10*d-5*e)/2)):
+                            total += 1
 
     return total
 
